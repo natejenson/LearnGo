c265d98cd8b9e2fb7707af2a40aa04af43a948f6
Nate Jenson <nate.m.jenson@gmail.com>
2017-10-11 20:43:50 -0500

    add unit test with test table

--- tests/sum_test.go
+++ tests/sum_test.go
@@ -10,3 +10,23 @@ func TestSum(t *testing.T) {
 		t.Errorf("Sum is incorrect. Expected %d, got %d", 10, total)
 	}
 }
+
+func TestABunchOfSums(t *testing.T) {
+	tables := []struct {
+		x   int32
+		y   int32
+		sum int64
+	}{
+		{1, 1, 2},
+		{2, 4, 6},
+		{100, 900, 1000},
+		{2000000000, 2000000000, 4000000000},
+	}
+
+	for _, table := range tables {
+		total := Sum(table.x, table.y)
+		if total != table.sum {
+			t.Errorf("Sum of %d and %d was incorrect. Expected %d, got %d", table.x, table.y, table.sum, total)
+		}
+	}
+}
